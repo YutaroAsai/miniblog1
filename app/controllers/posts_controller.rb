a5aab80b985f08e71d33a4390ad285b031ddaaf8
YutaroAsai <asari1991@gmail.com>
2024-07-23 17:40:15 +0900

    投稿フォーム実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,21 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: '投稿が成功しました。'
+    else
+      flash.now[:alert] = @post.errors.full_messages.join(', ')
+      render :index
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
